783cd23c4e65770189747f6bd87281be5891348f
one-west <wjdgkstj112@naver.com>
2024-12-31 00:16:07 +0900

    feat: Entity 추가
    - Movie
    - User
    - Review

---
+++ src/main/java/org/example/pickmovies/domain/User.java
@@ -0,0 +1,17 @@
+package org.example.pickmovies.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
