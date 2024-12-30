35e5679646c35cffb6cce6ef36e42622fa5ebbc6
one-west <wjdgkstj112@naver.com>
2024-12-30 20:20:47 +0900

    feat: Hello~Pick-Movies

---
+++ src/main/java/org/example/pickmovies/PickMoviesApplication.java
@@ -0,0 +1,13 @@
+package org.example.pickmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickMoviesApplication.class, args);
+	}
+
+}
