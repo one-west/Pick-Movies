58979d8756b795f7ad40908dd306d77bd27fed3b
one-west <wjdgkstj112@naver.com>
2025-01-09 02:51:47 +0900

    feat: 미인증 시 접근 제한

--- front/src/components/ProtectedRoute.tsx
+++ front/src/components/ProtectedRoute.tsx
@@ -1,17 +1,23 @@
+import React, {useEffect} from "react";
 import {Navigate} from "react-router-dom";
-import React from "react";
 
 interface ProtectedRouteProps {
   isAuthenticated: boolean;
-  children: React.ReactNode;
+  children: React.ReactElement;
 }
 
 // 로그인하지 않은 경우, 특정페이지 접근 막음
 export default function ProtectedRoute({isAuthenticated, children}: ProtectedRouteProps) {
-  if (isAuthenticated) {
-    return children;
-  } else {
-    return <Navigate to="/signin"/>;
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      alert("로그인이 필요합니다.");
+    }
+  }, [isAuthenticated])
+
+  if (!isAuthenticated) {
+    return <Navigate to="/signin" replace/>;
   }
+  return children;
 };
 
