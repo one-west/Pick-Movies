999cbaa1f9f2b6f6e0dc48adb29cfac8ebafc011
one-west <wjdgkstj112@naver.com>
2025-01-09 03:00:21 +0900

    feat: 사용자 프로필 초기화면

---
+++ front/src/pages/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+        <h1 className="text-4xl font-bold">Welcome to My Page!</h1>
+      </div>
+  );
+}
