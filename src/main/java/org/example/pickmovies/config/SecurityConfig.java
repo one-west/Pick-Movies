c2b92b0886bb6f3c311bb668443421bbd4015751
one-west <wjdgkstj112@naver.com>
2025-01-09 02:59:58 +0900

    refactor: 불필요한 내용 병합

--- src/main/java/org/example/pickmovies/config/SecurityConfig.java
+++ src/main/java/org/example/pickmovies/config/SecurityConfig.java
@@ -41,8 +41,6 @@ public class SecurityConfig {
         http.logout(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(new AntPathRequestMatcher("/api/movies/**")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/api/user/**")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/reviews/**")).authenticated()
                         .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/user/**")).hasRole("USER")
