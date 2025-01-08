9c8bf09db11f3c4ad5fdca7c7c07750071cd6e95
one-west <wjdgkstj112@naver.com>
2025-01-09 03:04:04 +0900

    refactor: 내용 변경
    - @PreAuthorize("hasRole('ADMIN')")

--- src/main/java/org/example/pickmovies/controller/AdminController.java
+++ src/main/java/org/example/pickmovies/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
-@PreAuthorize("hasRole('admin')")
+@PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
 public class AdminController {
 
