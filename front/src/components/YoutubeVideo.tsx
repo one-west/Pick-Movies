c47f288a59ef3b1b2365558fe3cb5081bb12f574
one-west <wjdgkstj112@naver.com>
2025-01-11 03:50:57 +0900

    fix: 에러 수정

--- front/src/components/YoutubeVideo.tsx
+++ front/src/components/YoutubeVideo.tsx
@@ -1,9 +1,13 @@
-export const YouTubeVideo = (videoId: string) => {
+interface VideoProps {
+  trailerKey: string;
+}
+
+export const YouTubeVideo = ({trailerKey}: VideoProps) => {
   return (
       <iframe
           width="100%"
           height="800"
-          src={`https://www.youtube.com/embed/${videoId}`}
+          src={`https://www.youtube.com/embed/${trailerKey}`}
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
