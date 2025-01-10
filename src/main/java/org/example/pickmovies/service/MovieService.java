b14037a5cb8cfbdbaad6e3396e29a4f4541a0cca
one-west <wjdgkstj112@naver.com>
2025-01-11 02:37:47 +0900

    feat: 영화 트레일러 정보 반환 메서드 추가

--- src/main/java/org/example/pickmovies/service/MovieService.java
+++ src/main/java/org/example/pickmovies/service/MovieService.java
@@ -18,6 +18,7 @@ public class MovieService {
     @Value("${tmdb.api-key}")
     private String apiKey;
 
+    // 인기 영화목록 가져오기
     public Mono<String> getPopularMovies() {
         String url = apiUrl + "/movie/popular?api_key=" + apiKey + "&language=ko&page=1";
 
@@ -28,6 +29,7 @@ public class MovieService {
                 .bodyToMono(String.class);
     }
 
+    // 개봉 예정 영화목록 가져오기
     public Mono<String> getUpcomingMovies() {
         String url = apiUrl + "/movie/upcoming?api_key=" + apiKey + "&language=ko";
 
@@ -38,8 +40,20 @@ public class MovieService {
                 .bodyToMono(String.class);
     }
 
+    // 영화 상세 정보 가져오기
     public Mono<String> getMovieDetails(Long id) {
         String url = apiUrl + "/movie/" + id + "?api_key=" + apiKey + "&language=ko";
+
+        return webClientBuilder.baseUrl(apiUrl).build()
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    // 영화 트레일러 정보를 가져오기
+    public Mono<String> getMovieVideos(Long id) {
+        String url = apiUrl + "/movie/" + id + "/videos" + "?api_key=" + apiKey + "&language=ko";
 
         return webClientBuilder.baseUrl(apiUrl).build()
                 .get()
