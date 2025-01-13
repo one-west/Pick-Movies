3864875095fd0d5fbc0705cc262f59976f299174
one-west <wjdgkstj112@naver.com>
2025-01-14 02:54:16 +0900

    refactor: 초기 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username, password, email, role, created_at)
-VALUES ('user1', '$2a$12$4pwTa4EG4.jMs.a8Od5BeevPbzuGm5YkBkoswUAtWuE3zYvoR.uH2', 'user@test.com', 'user', now());
+VALUES ('user1', '$2a$12$P/SNc/i2KUI3uzOFYdHJaeHIrY8EsK28lqDMoLOQr5l4VMX/8n6Lm', 'user@test.com', 'user', now());
 INSERT INTO users (username, password, email, role, created_at)
-VALUES ('admin', '$2a$12$4pwTa4EG4.jMs.a8Od5BeevPbzuGm5YkBkoswUAtWuE3zYvoR.uH2', 'admin@test.com', 'admin', now());
+VALUES ('admin', '$2a$12$P/SNc/i2KUI3uzOFYdHJaeHIrY8EsK28lqDMoLOQr5l4VMX/8n6Lm', 'admin@test.com', 'admin', now());
 
