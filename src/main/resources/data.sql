35c61e8dbef0b3a52411778743853599dba53c86
one-west <wjdgkstj112@naver.com>
2025-01-14 00:25:04 +0900

    feat: 초기 데이터 입력

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password, email, role, created_at)
+VALUES ('user1', '$2a$12$4pwTa4EG4.jMs.a8Od5BeevPbzuGm5YkBkoswUAtWuE3zYvoR.uH2', 'user@test.com', 'user', now());
+INSERT INTO users (username, password, email, role, created_at)
+VALUES ('admin', '$2a$12$4pwTa4EG4.jMs.a8Od5BeevPbzuGm5YkBkoswUAtWuE3zYvoR.uH2', 'admin@test.com', 'admin', now());
+
