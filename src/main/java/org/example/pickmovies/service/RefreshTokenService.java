6e5ee8df44ac1b309b187c220a2a254852f93e3e
one-west <wjdgkstj112@naver.com>
2025-01-15 00:42:57 +0900

    feat: RefreshTokenService 기능 추가
    - findByUserId() 메서드 추가

--- src/main/java/org/example/pickmovies/service/RefreshTokenService.java
+++ src/main/java/org/example/pickmovies/service/RefreshTokenService.java
@@ -11,8 +11,16 @@ public class RefreshTokenService {
 
     private final RefreshTokenRepository refreshTokenRepository;
 
+    // userId로 DB에서 refreshToken 찾기
+    public RefreshToken findByUserId(Long userId) {
+        return refreshTokenRepository.findByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Refresh token Not Found"));
+    }
+
+    // refreshToken으로 DB에서 refreshToken 찾기
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Refresh token Not Found"));
     }
+
 }
