6327e6ff223216b95b56039540ee7bd7086a1c09
one-west <wjdgkstj112@naver.com>
2025-01-15 00:33:11 +0900

    refactor: refreshToken 생성 기능 수정
    - DB, 쿠키 저장 로직 추가

--- src/main/java/org/example/pickmovies/service/TokenService.java
+++ src/main/java/org/example/pickmovies/service/TokenService.java
@@ -1,9 +1,14 @@
 package org.example.pickmovies.service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.example.pickmovies.domain.User;
 import org.example.pickmovies.jwt.JwtTokenProvider;
+import org.example.pickmovies.jwt.RefreshToken;
+import org.example.pickmovies.repository.RefreshTokenRepository;
+import org.example.pickmovies.utils.CookieUtil;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +18,14 @@ public class TokenService {
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
-    public String createAccessToken(String refreshToken) {
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(7);
+    public static final String REDIRECT_PATH = "/";
+
+    // 리프레시 토큰 생성 [AccessToken 갱신]
+    public String createRefreshToken(String refreshToken, Duration expiredAt) {
 
         // refreshToken 유효성 검사
         if (!jwtTokenProvider.validToken(refreshToken)) {
@@ -24,6 +35,27 @@ public class TokenService {
         Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
         User user = userService.findById(userId);
 
-        return jwtTokenProvider.generateToken(user, Duration.ofHours(2));
+        // RefreshToken DB에 저장
+        saveRefreshToken(user.getId(), refreshToken);
+
+        return jwtTokenProvider.generateToken(user, expiredAt);
+    }
+
+    // 생성된 리프레시 토큰을 쿠키에 저장
+    public void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+
+        int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.toSeconds();
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
+    }
+
+    // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장
+    public void saveRefreshToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository
+                .findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
     }
 }
