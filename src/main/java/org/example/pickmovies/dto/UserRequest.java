5027d5191b814d0cdc87b7ce4c747e876b11dcaf
one-west <wjdgkstj112@naver.com>
2025-01-18 01:56:51 +0900

    feat: 데이터 필드 추가

--- src/main/java/org/example/pickmovies/dto/UserRequest.java
+++ src/main/java/org/example/pickmovies/dto/UserRequest.java
@@ -4,8 +4,12 @@ import lombok.Data;
 
 @Data
 public class UserRequest {
+    // 회원가입 시
     private String username;
     private String email;
     private String password;
     private String role;
+
+    // 회원 정보 수정 시
+    private String newPassword;
 }
