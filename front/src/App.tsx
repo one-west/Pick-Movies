34a3db4a0410fcb79e7fdab90c0efa9792bc8e9c
one-west <wjdgkstj112@naver.com>
2025-01-20 23:59:34 +0900

    refactor: page 컴포넌트 네이밍 수정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,30 +1,40 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Signin from "./pages/Signin.tsx";
-import Signup from "./pages/Signup.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import SigninPage from "./pages/SigninPage.tsx";
+import SignupPage from "./pages/SignupPage.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
-import Profile from "./pages/Profile.tsx";
-import MovieDetail from "./pages/MovieDetail.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import MovieDetailPage from "./pages/MovieDetailPage.tsx";
 import {useEffect, useState} from "react";
 import {isTokenExpired, refreshAccessToken} from "./utils/authUtils.ts";
 import LoadingScreen from "./components/LoadingScreen.tsx";
 import Layout from "./components/Layout.tsx";
+import SearchPage from "./pages/SearchPage.tsx";
 
 export default function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false); // 로그인 상태 관리
 
   useEffect(() => {
-    const token = localStorage.getItem("accessToken");
+    const checkAuth = async () => {
+      const token = localStorage.getItem("accessToken");
 
-    if (token) {
-      if (isTokenExpired(token)) {
-        refreshAccessToken();
+      if (token) {
+        if (isTokenExpired(token)) {
+          const newToken = await refreshAccessToken();
+          setIsAuthenticated(!!newToken);
+
+        } else {
+          setIsAuthenticated(true);
+        }
+      } else {
+        setIsAuthenticated(false);
       }
-    }
+      setLoading(false);
+    };
 
-    setIsAuthenticated(!!token);
-    setLoading(false);
+    checkAuth();
+
   }, []); // 컴포넌트 마운트 시 실행
 
   const router = createBrowserRouter([
@@ -33,22 +43,26 @@ export default function App() {
       element: <Layout/>,
       children: [
         {
-          path: "/", element: <Home/>,
+          path: "/", element: <HomePage/>,
         },
         {
           path: "/movies/:movieId",
-          element:<MovieDetail isAuthenticated={isAuthenticated}/>
+          element: <MovieDetailPage isAuthenticated={isAuthenticated}/>
+        },
+        {
+          path: "/search",
+          element: <SearchPage />
         },
       ]
     },
     {
-      path: "/signin", element: <Signin setIsAuthenticated={setIsAuthenticated}/>,
+      path: "/signin", element: <SigninPage setIsAuthenticated={setIsAuthenticated}/>,
     },
     {
-      path: "/signup", element: <Signup/>,
+      path: "/signup", element: <SignupPage/>,
     },
     {
-      path: "/profile", element: <ProtectedRoute isAuthenticated={isAuthenticated}><Profile/></ProtectedRoute>
+      path: "/profile", element: <ProtectedRoute isAuthenticated={isAuthenticated}><ProfilePage/></ProtectedRoute>
     }
   ]);
 
