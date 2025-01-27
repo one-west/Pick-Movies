4b7e11972576a14850f4e7d88fa9a36505bdd792
one-west <wjdgkstj112@naver.com>
2025-01-27 19:10:48 +0900

    refactor: useContext로 상태 관리
    - 로그인 정보

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,82 +5,41 @@ import SignupPage from "./pages/SignupPage.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import MovieDetailPage from "./pages/MovieDetailPage.tsx";
-import {useEffect, useState} from "react";
-import {isTokenExpired, refreshAccessToken} from "./utils/authUtils.ts";
-import LoadingScreen from "./components/LoadingScreen.tsx";
 import Layout from "./components/Layout.tsx";
 import SearchPage from "./pages/SearchPage.tsx";
 import UpcomingPage from "./pages/UpcomingPage.tsx";
 import PopularPage from "./pages/PopularPage.tsx";
+import {AuthProvider} from "./context/AuthContext.tsx";
 
 export default function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // 로그인 상태 관리
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      const token = localStorage.getItem("accessToken");
-
-      if (token) {
-        if (isTokenExpired(token)) {
-          const newToken = await refreshAccessToken();
-          setIsAuthenticated(!!newToken);
-
-        } else {
-          setIsAuthenticated(true);
-        }
-      } else {
-        setIsAuthenticated(false);
-      }
-      setLoading(false);
-    };
-
-    checkAuth();
-
-  }, []); // 컴포넌트 마운트 시 실행
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout/>,
       children: [
-        {
-          path: "/", element: <HomePage/>,
-        },
-        {
-          path: "/movies/:movieId",
-          element: <MovieDetailPage isAuthenticated={isAuthenticated}/>
-        },
-        {
-          path: "/search",
-          element: <SearchPage />
-        },
-        {
-          path: "/popular",
-          element: <PopularPage />
-        },
-        {
-          path: "/upcoming",
-          element: <UpcomingPage />
-        },
-      ]
+        {path: "/", element: <HomePage/>},
+        {path: "/movies/:movieId", element: <MovieDetailPage/>},
+        {path: "/search", element: <SearchPage/>},
+        {path: "/popular", element: <PopularPage/>},
+        {path: "/upcoming", element: <UpcomingPage/>},
+      ],
     },
+    {path: "/signin", element: <SigninPage/>},
+    {path: "/signup", element: <SignupPage/>},
     {
-      path: "/signin", element: <SigninPage setIsAuthenticated={setIsAuthenticated}/>,
+      path: "/profile",
+      element: (
+          <ProtectedRoute>
+            <ProfilePage/>
+          </ProtectedRoute>
+      ),
     },
-    {
-      path: "/signup", element: <SignupPage/>,
-    },
-    {
-      path: "/profile", element: <ProtectedRoute isAuthenticated={isAuthenticated}><ProfilePage/></ProtectedRoute>
-    }
   ]);
 
-  return loading ? (
-      <LoadingScreen/>
-  ) : (
-      <div className="App">
+  return (
+      <AuthProvider>
         <RouterProvider router={router}/>
-      </div>
+      </AuthProvider>
   );
 }
