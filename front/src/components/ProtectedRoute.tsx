c9cb4ba8dba995152cd5b62b01e2be0e5d804cd4
one-west <wjdgkstj112@naver.com>
2025-01-27 19:15:47 +0900

    refactor: useContext로 상태 관리
    - 코드 간소화

--- front/src/components/ProtectedRoute.tsx
+++ front/src/components/ProtectedRoute.tsx
@@ -1,26 +1,14 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.tsx";
 
-interface ProtectedRouteProps {
-  isAuthenticated: boolean;
-  children: React.ReactElement;
-}
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  // 인증 정보 가져오기
+  const { isAuthenticated, loading } = useAuth();
 
-export default function ProtectedRoute({ isAuthenticated, children }: ProtectedRouteProps) {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      alert("로그인이 필요합니다.");
-      navigate("/signin", { state: { from: location.pathname } });
-    }
-  }, [isAuthenticated]);
-
-  // 인증되지 않은 경우 리디렉션 중이므로 null 반환
-  if (!isAuthenticated) {
-    return null;
+  if (loading) {
+    return <div>Loading...</div>; // 또는 로딩 화면
   }
 
-  // 인증된 경우 자식 컴포넌트를 렌더링
-  return children;
+  return isAuthenticated ? <>{children}</> : <Navigate to="/signin" />;
 }
